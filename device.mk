fd87645de3ede1c4cbc5fa599e6f2735636c533d
Eino-Ville Talvala <etalvala@google.com>
2014-07-15 11:05:18 -0700

    obiwan: Declare that this is a full-capability Camera2 device
     <feature name="android.hardware.camera.any" />
     <feature name="android.hardware.camera.level.full" />
     <feature name="android.hardware.camera.capability.manual_sensor" />
     <feature name="android.hardware.camera.capability.manual_post_processing" />
    
    Change-Id: I3ecaec9ac9519680842bb6de929c161cf6cd50dc

--- device.mk
+++ device.mk
@@ -107,6 +107,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
 # Camera
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
 PRODUCT_PACKAGES += \
