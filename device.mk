044aece9be200113db6f3a248d1b52d5fe830892
Alex Deymo <deymo@google.com>
2016-04-21 17:30:43 -0700

    obiwan: Build A/B update engine
    Change-Id: I9dbf84e1531ed3e32b21c8fec13266cfe4d23cd3

--- device.mk
+++ device.mk
@@ -123,5 +123,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
+# Update Engine
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
 # Vendor
 $(call inherit-product, vendor/asus/obiwan/obiwan-vendor.mk)
