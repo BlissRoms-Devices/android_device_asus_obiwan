ea21a6bf63a621535cb15d875f3a5731db14c0fd
Alex Wong <waiw@codeaurora.org>
2019-02-25 14:53:27 -0800

    obiwan: Build 64 bit camera service & implementation
    Change-Id: Ia098deaa9ea05b4f30747306b77d63fa4f3f5f90

--- device.mk
+++ device.mk
@@ -105,6 +105,11 @@ PRODUCT_PACKAGES += \
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64
+
 # Display
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
