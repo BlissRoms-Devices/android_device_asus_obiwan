b0520052b05bb00f3d57117c66a9ac84c2a47611
Subash Abhinov Kasiviswanathan <subashab@codeaurora.org>
2019-12-08 14:14:14 -0700

    obiwan: Build control library for rmnet
    Change-Id: Ifb1a8bae46c8c49911e1a5bffa26b969d92ec0a7

--- device.mk
+++ device.mk
@@ -105,6 +105,10 @@ PRODUCT_PACKAGES += \
 # Dalvik
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
+# Data
+PRODUCT_PACKAGES += \
+    librmnetctl
+
 # Camera
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
