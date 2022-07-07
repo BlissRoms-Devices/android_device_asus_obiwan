16c6df056ed4ad3312046a6c99b3fab858c4b4f5
Jackeagle <jackeagle102@gmail.com>
2022-07-07 10:44:45 -0400

    obiwan: Unify overlays
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- device.mk
+++ device.mk
@@ -36,7 +36,8 @@ PRODUCT_EXTRA_VNDK_VERSIONS := 30
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-lineage \
+    $(LOCAL_PATH)/overlay-bliss
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
