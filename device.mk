6cf2e6b7f5060c45147d6ac225e7864f8ccb3e0c
Alexander Koskovich <akoskovich@pm.me>
2022-08-10 02:30:07 -0700

    obiwan: Build the fingerprint service and declare support
    Change-Id: Ic8eb351a4dd682efd2442c1486dc49a528887157

--- device.mk
+++ device.mk
@@ -85,6 +85,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.authsecret@1.0.vendor
 
+# Biometrics
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service
+
 # Bluetooth
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml \
