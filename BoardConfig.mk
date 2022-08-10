88ba0424303975c9ddd2cb661e77a0f7bd587447
Alexander Koskovich <akoskovich@pm.me>
2022-08-09 20:09:15 -0700

    obiwan: Import config.fs from LA.UM.9.12.r1-14400.02-SMxx50.QSSI13.0
    Change-Id: I589bd22f5796a7e087c364930086094621c33b69

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,6 +61,9 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 # Display
 TARGET_SCREEN_DENSITY := 420
 
+# FSConfig
+TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
+
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
