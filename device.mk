1d84c515362c48ef290f936a4aed00c086e270a1
Jackeagle <jackeagle102@gmail.com>
2022-08-26 12:04:34 -0400

    obiwan: Copy prebuilt libhidlbase-v32.so
    - Required for fingerprint hw
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- device.mk
+++ device.mk
@@ -419,6 +419,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator.service.obiwan
 
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
+
 # WFD
 PRODUCT_BOOT_JARS += \
     WfdCommon
