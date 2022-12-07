36eb4014ebe7736ffd3de7140c780e552d2a0501
Bruno Martins <bgcngm@gmail.com>
2022-12-07 11:03:15 +0800

    obiwan: AsusParts: Fix compilation for QPR1 merge
    Change-Id: I71ad7db85592ca18e3f0f8155bba5a472bfadbaa

--- AsusParts/src/org/blissroms/settings/asusparts/AsusPartsActivity.java
+++ AsusParts/src/org/blissroms/settings/asusparts/AsusPartsActivity.java
@@ -22,7 +22,7 @@ import android.os.Bundle;
 
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.collapsingtoolbar.R;
+import com.android.settingslib.widget.R;
 
 public class AsusPartsActivity extends CollapsingToolbarBaseActivity {
 
