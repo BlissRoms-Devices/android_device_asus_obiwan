d7bd4ad346198ba80c9dc45dbb27284187eb17b0
Jackeagle <jackeagle102@gmail.com>
2023-10-11 23:11:22 -0400

    obiwan: Provide android.frameworks.sensorservice@1.0 on vendor
    - Fixed Camera App FC on firing it up.
    E vndksupport: Could not load /vendor/lib64/hw/camera.qcom.so from default namespace: dlopen failed: library "android.frameworks.sensorservice@1.0.so" not found: needed by /vendor/lib64/hw/camera.qcom.so in namespace (default).
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- device.mk
+++ device.mk
@@ -155,7 +155,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
     vendor.qti.hardware.camera.device@1.0.vendor \
-    vendor.qti.hardware.camera.postproc@1.0.vendor
+    vendor.qti.hardware.camera.postproc@1.0.vendor \
+    android.frameworks.sensorservice@1.0.vendor \
 
 # Display
 PRODUCT_COPY_FILES += \
