52dcf27d788a1cdaeda1b29a6f3d5b5fe668941b
Vaisakh Murali <vaisakhmurali@gmail.com>
2024-06-16 06:13:34 -0400

    obiwan: Build vendor variants of some dependencies
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- device.mk
+++ device.mk
@@ -153,6 +153,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor \
     android.frameworks.sensorservice@1.0.vendor \
+    libexif.vendor \
+    libpng.vendor \
 
 # DebugFS
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
@@ -357,6 +359,7 @@ TARGET_FWK_SUPPORTS_FULL_VALUEADDS := true
 
 PRODUCT_PACKAGES += \
     libjson \
+    libjsoncpp.vendor \
     libqti_vndfwk_detect.vendor \
     libvndfwk_detect_jni.qti.vendor
 
@@ -365,7 +368,8 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
-    android.hardware.secure_element@1.2.vendor
+    android.hardware.secure_element@1.2.vendor \
+    libcurl.vendor \
 
 # Sensors
 PRODUCT_COPY_FILES += \
@@ -386,6 +390,10 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 
 # Storage
 PRODUCT_CHARACTERISTICS := nosdcard
+
+# SQLite
+PRODUCT_PACKAGES += \
+    libsqlite.vendor
 
 # Telephony
 PRODUCT_COPY_FILES += \
